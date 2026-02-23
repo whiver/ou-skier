d49440ff35e0e3e6f6091c4d1170d51b646fcb42
William Hiver <william.hiver@free.fr>
2026-02-23 23:41:33 +0100

    Fix missing generated prisma

--- web/lib/prisma.ts
+++ web/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaClient } from "@prisma/client/index";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
